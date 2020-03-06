84000298f0e22cd444f2baafbf5e6ca962166e04
Marvbell110 <34713144+Marvbell110@users.noreply.github.com>
2020-03-06 18:34:16 +0100

    Add french locales

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -12,6 +12,7 @@ server_scripts {
 	'locales/en.lua',
 	'locales/cs.lua',
 	'locales/de.lua',
+	'locales/fr.lua',
 	'config.lua',
 	'server/main.lua'
 }
@@ -21,6 +22,7 @@ client_scripts {
 	'locales/en.lua',
 	'locales/cs.lua',
 	'locales/de.lua',
+	'locales/fr.lua',
 	'config.lua',
 	'client/main.lua'
 }
