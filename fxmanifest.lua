47cd006aedb1be2d5b824b12a374bc2f137a88d2
Marvbell110 <34713144+Marvbell110@users.noreply.github.com>
2020-03-06 17:19:52 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,36 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'ESX Identity'
+
+version '1.2.0'
+
+server_scripts {
+	'@es_extended/locale.lua',
+	'@mysql-async/lib/MySQL.lua',
+	'locales/en.lua',
+	'locales/cs.lua',
+	'locales/de.lua',
+	'config.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'locales/cs.lua',
+	'locales/de.lua',
+	'config.lua',
+	'client/main.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+	'html/index.html',
+	'html/script.js',
+	'html/style.css'
+}
+
+dependency 'es_extended'
